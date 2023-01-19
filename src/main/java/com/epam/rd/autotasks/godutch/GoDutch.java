7707d49d511cc8a68a0030fb9804164d5a5fa4c7
Ashish7429 <ashish.11916604@lpu.in>
2023-01-19 17:01:59 +0530

    dfnm

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,21 @@
 package com.epam.rd.autotasks.godutch;
 
+import java.util.Scanner;
+
 public class GoDutch {
 
     public static void main(String[] args) {
         //Write code here
+        Scanner sc = new Scanner(System.in);
+        int bill,friends;
+        bill = sc.nextInt();
+        friends = sc.nextInt();
+        if(bill<0){
+            System.out.println("Bill total amount cannot be negative");
+        }else if(friends<=0){
+            System.out.println("Number of friends cannot be negative or zero");
+        }else{
+            System.out.println(bill/friends);
+        }
     }
 }
